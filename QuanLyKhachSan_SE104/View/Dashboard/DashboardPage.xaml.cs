19a81e861b3234e21d707ed7c7071d7408967d98
nghi1711 <152877364+nghi1711@users.noreply.github.com>
2026-04-12 21:17:57 +0700

    Update DashboardPage.xaml.cs

--- QuanLyKhachSan_SE104/View/Dashboard/DashboardPage.xaml.cs
+++ QuanLyKhachSan_SE104/View/Dashboard/DashboardPage.xaml.cs
@@ -1,30 +1,15 @@
-﻿using QuanLyKhachSan_SE104.Model;
-using QuanLyKhachSan_SE104.ViewModel;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+using QuanLyKhachSan_SE104.ViewModel.Dashboard;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace QuanLyKhachSan_SE104.View.Dashboard 
+namespace QuanLyKhachSan_SE104.View.Dashboard
 {
-    public partial class DashboardPage : UserControl 
+    public partial class DashboardPage : UserControl
     {
         public DashboardPage()
         {
             InitializeComponent();
-            this.DataContext = new QuanLyKhachSan_SE104.ViewModel.Dashboard.DashboardViewModel();
+            this.DataContext = new DashboardViewModel();
         }
-
     }
 }
