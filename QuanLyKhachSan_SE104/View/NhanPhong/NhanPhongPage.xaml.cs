2be7c8370a7006dbe73cbad3610d26f59b74feaf
Thao-Tien <lilianjung2036@gmail.com>
2026-04-21 23:36:54 +0700

    Feat: nhanphong, add DAL, DTO

--- QuanLyKhachSan_SE104/View/NhanPhong/NhanPhongPage.xaml.cs
+++ QuanLyKhachSan_SE104/View/NhanPhong/NhanPhongPage.xaml.cs
@@ -16,28 +16,28 @@ namespace QuanLyKhachSan_SE104.View.NhanPhong
             this.DataContext = new NhanPhongViewModel();
         }
 
-        private void ShowPopup_Click(object sender, RoutedEventArgs e)
-        {
-            PopupOverlay.Visibility = Visibility.Visible;
-        }
+        //private void ShowPopup_Click(object sender, RoutedEventArgs e)
+        //{
+        //    PopupOverlay.Visibility = Visibility.Visible;
+        //}
 
-        private void ClosePopup_Click(object sender, RoutedEventArgs e)
-        {
-            PopupOverlay.Visibility = Visibility.Collapsed;
-        }
+        //private void ClosePopup_Click(object sender, RoutedEventArgs e)
+        //{
+        //    PopupOverlay.Visibility = Visibility.Collapsed;
+        //}
 
-        private void ShowChiTiet_Click(object sender, RoutedEventArgs e)
-        {
-            if (sender is Button btn && btn.Tag is ModelChiTiet chiTiet)
-            {
-                if (chiTiet.DatPhong == null) return;
+        //private void ShowChiTiet_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (sender is Button btn && btn.Tag is ModelChiTiet chiTiet)
+        //    {
+        //        if (chiTiet.DatPhong == null) return;
 
-                var win = new ChiTietDatPhongWindow(chiTiet.DatPhong)
-                {
-                    Owner = Window.GetWindow(this)
-                };
-                win.ShowDialog();
-            }
-        }
+        //        var win = new ChiTietDatPhongWindow(chiTiet.DatPhong)
+        //        {
+        //            Owner = Window.GetWindow(this)
+        //        };
+        //        win.ShowDialog();
+        //    }
+        //}
     }
 }
