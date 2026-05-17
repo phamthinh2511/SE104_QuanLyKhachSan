08c807ddd77cae7d433accbd30f07fb1bfba035e
phamthinh2511 <flare.dsly@gmail.com>
2026-05-17 12:48:35 +0700

    Fix

--- QuanLyKhachSan_SE104/View/ChiTietDatPhongView/ChiTietDatPhongPage.xaml.cs
+++ QuanLyKhachSan_SE104/View/ChiTietDatPhongView/ChiTietDatPhongPage.xaml.cs
@@ -1,19 +1,5 @@
-﻿using QuanLyKhachSan_SE104.View.DatPhong;
-using QuanLyKhachSan_SE104.ViewModel.ChiTietDatPhong;
-using QuanLyKhachSan_SE104.ViewModel.MainViewModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using QuanLyKhachSan_SE104.ViewModel.ChiTietDatPhongVM;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace QuanLyKhachSan_SE104.View.ChiTietDatPhongView
 {
